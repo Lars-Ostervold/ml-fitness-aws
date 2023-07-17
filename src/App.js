32c63892a9f696acd43fbe36812d206544e3fca3
Lars-Ostervold <ostervold.berent@gmail.com>
2023-07-16 20:03:27 -0500

    Begin mui

--- src/App.js
+++ src/App.js
@@ -1,170 +1,183 @@
-import logo from './logo.svg';
 import './App.css';
+import { Box, Button, Typography, Select, MenuItem } from '@mui/material'
 import { API } from 'aws-amplify';
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import TableComponent from './TableComponent';
-// import { Lambda } from 'aws-sdk';
 
 
 function App() {
-  const [selectedOption1, setSelectedOption1] = useState('');
-  const [selectedOption2, setSelectedOption2] = useState('');
-  const [selectedOption3, setSelectedOption3] = useState('');
-  const [selectedOption4, setSelectedOption4] = useState('');
+  const [daysPerWeekOption, setdaysPerWeekOption] = useState('');
+  const [timePerSessionOption, settimePerSessionOption] = useState('');
+  const [fitnessGoalOption, setfitnessGoalOption] = useState('');
+  const [userExperienceOption, setuserExperienceOption] = useState('');
   const [exerciseData, setExerciseData] = useState([]);
+  const [availableDaysPerWeekOptions, setAvailableDaysPerWeekOptions] = useState([
+    "1", "2", "3", "4", "5", "6", "7"
+  ]);
 
-  // const handleSubmit = async () => {
-  //   const lambda = new Lambda({ region: 'us-east-1' }); // Update the region as per your Lambda function's region
+  const handleUserExperienceChange = (e) => {
+    const selectedValue = e.target.value;
+    setuserExperienceOption(selectedValue);
   
-  //   const payload = {
-  //     option1: selectedOption1,
-  //     option2: selectedOption2,
-  //     option3: selectedOption3,
-  //     option4: selectedOption4
-  //   };
+    // Update the available options for "Days per Week" based on the selected value
+    if (selectedValue === "1") {
+      setAvailableDaysPerWeekOptions(["1", "2", "3"]);
+    } else if (selectedValue === "2") {
+      setAvailableDaysPerWeekOptions(["1", "2", "3", "4", "5"]);
+    } else {
+      setAvailableDaysPerWeekOptions(["1", "2", "3", "4", "5", "6", "7"]);
+    }
+  };
   
-  //   const params = {
-  //     FunctionName: 'YourLambdaFunctionName',
-  //     Payload: JSON.stringify(payload)
-  //   };
-  
-  //   try {
-  //     const response = await lambda.invoke(params).promise();
-  //     const data = JSON.parse(response.Payload);
-  //     console.log(data);
-  //     setExerciseData(data); // Example: Assuming the response is JSON data
-  //     console.log('Here is the exercise data:');
-  //     console.log(exerciseData);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
 
   const handleSubmit = async () => {
     const payload = {
-      option1: selectedOption1,
-      option2: selectedOption2,
-      option3: selectedOption3,
-      option4: selectedOption4
+      option1: daysPerWeekOption,
+      option2: timePerSessionOption,
+      option3: fitnessGoalOption,
+      option4: userExperienceOption
     };
+
+    //----------------------LAMBDA--------------------------------------
+    // try { //Call the API using an amplify package and send 'payload'
+    //   const response = await API.post('generateworkoutAPI', '/workout', {
+    //     body: payload //If need to specify method, can send an 'httpMethod' header with payload
+    //   });
+
+    //   //Get status code from backend
+    //   const status_check = response.statusCode
+
+    //   // The backend outputs a dictionary, but stores it as a string by default.
+    //   // The .parse function will convert it into an indexed list
+    //   const data = JSON.parse(response.body);
+      
+    //   if (status_check === 400) {//400 error is unselected options for now
+    //     setExerciseData(response.body.replace(/"/g, ''));
+    //   } else{ 
+    //     //This command takes the indexed list (indexed by key for every value) and
+    //     //Converts into an array so that the .map in TableComponent will work.
+    //     const exerciseArray = Object.entries(data).map(([key, value]) => ({
+    //       id: key,
+    //       exercises: value,
+    //     }));
     
-    // const url = 'https://dy9s1q0nb8.execute-api.us-east-1.amazonaws.com/dev/workout'
-    // const headers = {'Content-Type': 'application/json'};
-    // const body = JSON.stringify({ payload });
+        
+    //     //Set the data to the array to pass later
+    //     setExerciseData(exerciseArray);
+    //   }   
 
-    try {
-      const response = await API.post('generateworkoutAPI', '/workout', {
-        body: payload
+    // } catch (error) {
+    //   console.error(error);
+    // };
+
+    //--------------------------FLASK-----------------------------------------
+    // For calling the flask app directly (local dev)
+    const url = 'http://127.0.0.1:5000/workout';
+    const headers = {'Content-Type': 'application/json'};
+    const body = JSON.stringify({ payload });
+    fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body: body
+    })
+      .then((response) => response.json()) 
+      
+      // Parse the response as JSON
+      //With fetch, response.body can't be called directly, so we parse response
+      //with response.json() first, then reponse gets stored as whatever we call
+      //in the next arrow function (data in this case), which we can then access
+      //body with data.body
+      .then((data) => { 
+        const status_check = data.statusCode //Get status code from backend
+        const data2 = JSON.parse(data.body)
+
+        if (status_check === 400) {//400 error is unselected options for now
+          setExerciseData(data.body.replace(/"/g, ''));//Returns error w/o double quotes
+        } else{ 
+          const exerciseArray = Object.entries(data2).map(([key, value]) => ({
+            id: key,
+            exercises: value,
+          }));
+  
+      
+          setExerciseData(exerciseArray);
+        }
+
+
+      })
+      .catch((error) => {
+        // Handle the error
+        console.error(error);
       });
-      // console.log('This is type of response:', typeof response)
-      // console.log(response)
-      // console.log('This is response.body:')
-      // console.log(response.body)
-      // console.log('this is type of body:', typeof response.body)
 
-
-      const data = JSON.parse(response.body);
-      console.log(data)
-
-      const exerciseArray = Object.entries(data).map(([key, value]) => ({
-        id: key,
-        exercises: value,
-      }));
-      console.log(exerciseArray);
-
-      setExerciseData(exerciseArray);
-      console.log(exerciseData)
-    } catch (error) {
-      console.error(error);
-    };
-
-    // For calling the flask app directly
-    //const url = 'http://127.0.0.1:5000/workout'; //for local dev
-    // fetch(url, {
-    //   method: 'POST',
-    //   headers: headers,
-    //   body: body
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log(data)
-    //     setExerciseData(data); // Example: Assuming the response is JSON data
-    //     console.log('Here is the excercise data:')
-    //     console.log(exerciseData)
-    //   })
-    //   .then((sortedList) => {
-    //     // Handle the sorted list response
-    //   })
-    //   .catch((error) => {
-    //     // Handle the error
-    //     console.error(error);
-    //   });
-
-    // await API.post("generateworkoutAPI", "/workout", {'Content-Type': 'application/json', 'Accept': 'application/json', data} )
-    //   .then((response) => {
-    //     // Handle the response from the backend
-    //     console.log(response)
-    //     setExerciseData(response.data); // Example: Assuming the response is JSON data
-    //     console.log('Here is the excercise data:')
-    //     console.log(exerciseData)
-    //   })
-    //   .catch((error) => {
-    //     // Handle any error that occurs during the API request
-    //     console.error(error);
-    //   });
   };
   
 
   return (
     <div className="App">
-      <div className="welcome-message">
-        <h1>Hi! Select the settings below and click the 'Generate Workout' button to get started!</h1>
-      </div>
-      <div className="dropdowns">
-        <select name="option1" value={selectedOption1} onChange={(e) => setSelectedOption1(e.target.value)}>
-          <option value="">Days per week</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          {/* Add more options as needed */}
-        </select>
-        <select name="option2" value={selectedOption2} onChange={(e) => setSelectedOption2(e.target.value)}>
-          <option value="">Time per Session</option>
-          <option value="15">15</option>
-          <option value="30">30</option>
-          <option value="45">45</option>
-          <option value="60">60</option>
-          {/* Add more options as needed */}
-        </select>
-        <select name="option3" value={selectedOption3} onChange={(e) => setSelectedOption3(e.target.value)}>
-          <option value="">Fitness Goal</option>
-          <option value="1">Strength</option>
-          <option value="2">Body Building</option>
-          <option value="3">Lean</option>
-          {/* Add more options as needed */}
-        </select>
-        <select name="option4" value={selectedOption4} onChange={(e) => setSelectedOption4(e.target.value)}>
-          <option value="">Gym Experience</option>
-          <option value="1">Beginner</option>
-          <option value="2">Intermediate</option>
-          <option value="3">Expert</option>
-          {/* Add more options as needed */}
-        </select>
-      </div>
-
-      <button className="submit-button" onClick={handleSubmit}>
-        Generate Workout
-      </button>
-    
-
-      <TableComponent exerciseData={exerciseData} />
-
+      <Box sx={{ padding: 2 }}>
+        <Typography variant="h1">
+          Hi! Select the settings below and click the 'Generate Workout' button to get started!
+        </Typography>
+        <Box sx={{ display: 'flex', gap: 2, marginBottom: 2 }}>
+          <div>
+            <Select
+              name="userExperience"
+              value={userExperienceOption}
+              onChange={handleUserExperienceChange}
+            >
+              <MenuItem value="1">Beginner</MenuItem>
+              <MenuItem value="2">Intermediate</MenuItem>
+              <MenuItem value="3">Expert</MenuItem>
+            </Select>
+            <Typography variant="h6">Gym Experience</Typography>
+          </div>
+          <div>
+            <Select
+              name="daysPerWeek"
+              value={daysPerWeekOption}
+              onChange={(e) => setdaysPerWeekOption(e.target.value)}
+            >
+              {availableDaysPerWeekOptions.map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </Select>
+            <Typography variant="h6">Days per Week</Typography>
+          </div>
+          <div>
+            <Select
+              name="timePerSession"
+              value={timePerSessionOption}
+              onChange={(e) => settimePerSessionOption(e.target.value)}
+            >
+              <MenuItem value="15">15</MenuItem>
+              <MenuItem value="30">30</MenuItem>
+              <MenuItem value="45">45</MenuItem>
+              <MenuItem value="60">60</MenuItem>
+            </Select>
+            <Typography variant="h6">Time per Session</Typography>
+          </div>
+          <div>
+            <Select
+              name="fitnessGoal"
+              value={fitnessGoalOption}
+              onChange={(e) => setfitnessGoalOption(e.target.value)}
+            >
+              <MenuItem value="1">Strength</MenuItem>
+              <MenuItem value="2">Body Building</MenuItem>
+              <MenuItem value="3">Lean</MenuItem>
+            </Select>
+            <Typography variant="h6">Fitness Goal</Typography>
+          </div>
+        </Box>
+        <Button variant="contained" onClick={handleSubmit}>
+          Generate Workout
+        </Button>
+        <TableComponent exerciseData={exerciseData} />
+      </Box>
     </div>
   );
 }
